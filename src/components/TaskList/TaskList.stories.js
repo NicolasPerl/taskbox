86558188de58a1ffe7e2b404901ce459154a07a3
nicolas-accpl <nicolas.perlick@accpl.co>
2021-05-19 16:54:19 -0500

    removed red color for title and added example of long string

--- src/components/TaskList/TaskList.stories.js
+++ src/components/TaskList/TaskList.stories.js
@@ -24,6 +24,16 @@ Default.args = {
   ],
 };
 
+const longstring = `This task's name is absurdly large. In fact, I think if I keep going I might end up with content overflow. What will happen? The star that represents a pinned task could have text overlapping. The text could cut-off abruptly when it reaches the star. I hope not!`
+export const LongTitleString = Template.bind({})
+
+LongTitleString.args = {
+  tasks: [
+    ...Default.args.tasks,
+    {...TaskStories.Default.args.task, id: '7', title: longstring, state: 'TASK_PINNED'}
+  ]
+}
+
 export const WithPinnedTasks = Template.bind({});
 WithPinnedTasks.args = {
   // Shaping the stories through args composition.
